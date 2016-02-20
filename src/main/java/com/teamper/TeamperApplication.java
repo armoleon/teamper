2f149d3b80c30ca290833f512e9f06b287038ec7
armoleon <okayas@gmail.com>
2016-02-20 20:01:47 +0200

    first commit

---
+++ src/main/java/com/teamper/TeamperApplication.java
@@ -0,0 +1,12 @@
+package com.teamper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamperApplication.class, args);
+	}
+}
